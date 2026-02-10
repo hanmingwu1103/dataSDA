e9872fd3d0215f134b15124d3b9838275723a3e6
Wu, Han-Ming (Hank) <wuhm@g.nccu.edu.tw>
2026-02-11 00:11:33 +0800

    Merge clean_colnames.R into interval_utils.R and rename write_csv_table.R to utilities.R
    Used-tool: Claude Opus 4.6

--- R/interval_utils.R
+++ R/interval_utils.R
@@ -157,6 +157,27 @@ Interval_to_FV <- function(idata) {
 
 # --- Format preparation functions ---------------------------------------------
 
+#' clean_colnames
+#'
+#' @name clean_colnames
+#' @aliases clean_colnames
+#' @description This function is used to clean up variable names to conform to the RSDA format.
+#' @usage clean_colnames(data)
+#' @param data The conventional data.
+#' @returns Data after cleaning variable names.
+#' @examples
+#' data(mushroom)
+#' mushroom.clean <- clean_colnames(data = mushroom)
+#' @export
+
+clean_colnames <- function(data){
+  .check_data_frame(data, "clean_colnames")
+  colnames(data) <- gsub("_min|_max|_Min|_Max|.min|.max|.Min|.Max",
+                         '', colnames(data))
+  return(data)
+}
+
+
 #' RSDA Format
 #'
 #' @name RSDA_format
